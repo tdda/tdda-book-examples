72c5e27fe24a534034c6bd5695bf91f1a3e0b20b
Nick Radcliffe <njr@StochasticSolutions.com>
2026-03-31 11:38:55 +0100

    .

---
+++ c5/discover-a1k.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This is identical to discover-accounts1k.sh, which is the file
+# actually used in the book's preparation, apart from this comment.
+# This duplicate file has been created as a duplicate because
+# the wrong link was put in the print copy of the book
+# This is corrected in the online copy.
+tdda discover -xG accounts1k.csv accounts.tdda
