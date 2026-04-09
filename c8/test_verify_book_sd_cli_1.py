e72ad5b9dd5d6f5de6cc8f665b68ec3c54dd2253
Nick Radcliffe <njr@StochasticSolutions.com>
2026-04-09 11:37:11 +0100

    More reference TDDD for c5 and c8

---
+++ c8/test_verify_book_sd_cli_1.py
@@ -0,0 +1,47 @@
+"""
+test_verify_book_sd_cli_1.py: Automatically generated test code from tdda gentest.
+
+Generation command:
+
+tdda gentest 'tdda verify elements3-old.parquet elements118.tdda --no-varf' 'test_verify_book_sd_cli_1.py' '.'
+"""
+
+import os
+import sys
+import tempfile
+
+from tdda.referencetest import ReferenceTestCase
+from tdda.referencetest.gentest import exec_command
+
+
+class Test_VERIFY_BOOK_SD_CLI_1(ReferenceTestCase):
+    command = 'tdda verify elements3-old.parquet elements118.tdda --no-varf'
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    refdir = os.path.join(cwd, 'ref', 'verify_book_sd_cli_1')
+
+
+    @classmethod
+    def setUpClass(cls):
+        
+        (cls.output,
+         cls.error,
+         cls.exception,
+         cls.exit_code,
+         cls.duration) = exec_command(cls.command, cls.cwd)
+
+    def test_no_exception(self):
+        self.assertIsNone(self.exception)
+
+    def test_exit_code(self):
+        self.assertEqual(self.exit_code, 0)
+
+    def test_stdout(self):
+        self.assertStringCorrect(self.output,
+                                 os.path.join(self.refdir, 'STDOUT'))
+
+    def test_stderr(self):
+        self.assertStringCorrect(self.error,
+                                 os.path.join(self.refdir, 'STDERR'))
+
+if __name__ == '__main__':
+    ReferenceTestCase.main()
